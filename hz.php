c9e32dea0f7892f32033b5e420c1212da97426b6
dimasikpsk <139615397+dimasikpsk@users.noreply.github.com>
2024-08-18 12:57:40 +0300

    Update hz.php

--- hz.php
+++ hz.php
@@ -19,13 +19,7 @@ unset($a[2]);
  dsvbgnhmbnjkyrhrqencgbarhtht  bgnscdvfvdv
  
  
- vdsvdk;vvpkdgrg
- rrgrg
- ggrgrrhjj
- yrk
- вымsddds  fef= 1;
-  while ($i < 1050) {
-    echo $i . ' шаг цикла<br>';
+ 
     $i = $i + 1;
   }
   <?php
